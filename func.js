d6482fb18e64ef3afd269c0fcf9d3c686249349d
t2023-m0091 <t2023-m0091@t2023-m0091ui-MacBookAir.local>
2024-05-01 16:40:04 +0900

    test2

--- func.js
+++ func.js
@@ -1,5 +1,5 @@
 let movies = []; // API에서 가져온 영화 데이터를 저장할 배열 movies 전역 변수로 선언하여 다른 함수들도 접근 가능하도록 함
-//najiji
+//najiji test2222
 document.addEventListener('DOMContentLoaded', function() {
     const scrollContainer = document.getElementById('movies-container');
 
