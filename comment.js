ced54522bb2bcc2391404e5397cac1d0a6be41ef
unknown <mgj2630@gmail.com>
2024-05-02 17:53:31 +0900

    valicationtest파일 확인

---
+++ comment.js
@@ -0,0 +1,12 @@
+<form>
+  <label htmlFor="name">
+    <div>Name</div>
+    <input
+      type="text"
+      id="name"
+      name="user_name"
+      placeholder="성함을 입력해주세요."
+      required
+    />
+  </label>
+</form>;
