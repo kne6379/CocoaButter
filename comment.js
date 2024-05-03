25815c806cacd51834bb591f48f9061f9fffed9c
unknown <mgj2630@gmail.com>
2024-05-03 17:29:15 +0900

    js파일 업데이트

--- comment.js
+++ comment.js
@@ -1,12 +1,16 @@
-<form>
-  <label htmlFor="name">
-    <div>Name</div>
-    <input
-      type="text"
-      id="name"
-      name="user_name"
-      placeholder="성함을 입력해주세요."
-      required
-    />
-  </label>
-</form>;
+function check() {
+  let pass = document.getElementById("pass");
+  let form = document.getElementById("form");
+  let RegExp =
+    /^(?=.*[A-Z])|(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{4,}$/;
+
+  if (pass.value.length == 0) {
+    alert("비밀번호를 입력하세요");
+    pass.focus();
+  } else if (!RegExp.exec(pass.value)) {
+    alert("비밀번호를 4자리 이상, 대문자나 특수문자 포함하여 입력하세요");
+    pass.focus();
+  }
+}
+let checkBtn = document.getElementById("checkBtn");
+checkBtn.addEventListener("click", check);
