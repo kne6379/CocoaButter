ffd60e7167cf76d250b04bf0d8e292c1e005ca94
unknown <mgj2630@gmail.com>
2024-05-03 17:50:37 +0900

    비밀번호생성,댓글작성시 비밀번호 확인 유효성검사

--- comment.js
+++ comment.js
@@ -1,8 +1,9 @@
+// 정규식을 활용한 댓글 작성시 비밀번호입력 유효성 검사
+let RegExp = /^(?=.*[A-Z])|(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{4,}$/;
+
 function check() {
   let pass = document.getElementById("pass");
   let form = document.getElementById("form");
-  let RegExp =
-    /^(?=.*[A-Z])|(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{4,}$/;
 
   if (pass.value.length == 0) {
     alert("비밀번호를 입력하세요");
@@ -10,7 +11,14 @@ function check() {
   } else if (!RegExp.exec(pass.value)) {
     alert("비밀번호를 4자리 이상, 대문자나 특수문자 포함하여 입력하세요");
     pass.focus();
+  } else {
+    alert("댓글 작성이 완료되었습니다.");
   }
 }
+// 버튼 클릭 이벤트
 let checkBtn = document.getElementById("checkBtn");
 checkBtn.addEventListener("click", check);
+// 비밀번호 생성 유효성 검사
+function validatePassword(newpassword) {
+  return RegExp.test(newpassword);
+}
