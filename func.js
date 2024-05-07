15bad051deaa884a051fa59877b5bd6540d83d41
unknown <mgj2630@gmail.com>
2024-05-07 12:41:34 +0900

    뒤로가기버튼 추가

--- func.js
+++ func.js
@@ -1,5 +1,4 @@
 let movies = []; // API에서 가져온 영화 데이터를 저장할 배열 movies 전역 변수로 선언하여 다른 함수들도 접근 가능하도록 함
-//najiji test2222 test 3
 document.addEventListener("DOMContentLoaded", function () {
   const scrollContainer = document.getElementById("movies-container");
 
