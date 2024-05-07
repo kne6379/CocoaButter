59a01d17ff8da60e36c8d6879549b7f1160b79e4
unknown <mgj2630@gmail.com>
2024-05-07 12:47:14 +0900

    뒤로가기버튼 추가

--- comment.js
+++ comment.js
@@ -1,24 +1,33 @@
-// 정규식을 활용한 댓글 작성시 비밀번호입력 유효성 검사
-let RegExp = /^(?=.*[A-Z])|(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{4,}$/;
+// 정규식을 활용한 댓글 작성시 비밀번호 입력 유효성 검사
+const passwordRegex =
+  /^(?=.*[A-Z])|(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{4,}$/;
 
-function check() {
-  let pass = document.getElementById("pass");
-  let form = document.getElementById("form");
+function checkPassword() {
+  const passwordInput = document.getElementById("pass");
+  const form = document.getElementById("form");
 
-  if (pass.value.length == 0) {
+  if (passwordInput.value.length === 0) {
     alert("비밀번호를 입력하세요");
-    pass.focus();
-  } else if (!RegExp.exec(pass.value)) {
+    passwordInput.focus();
+  } else if (!passwordRegex.test(passwordInput.value)) {
     alert("비밀번호를 4자리 이상, 대문자나 특수문자 포함하여 입력하세요");
-    pass.focus();
+    passwordInput.focus();
   } else {
     alert("댓글 작성이 완료되었습니다.");
   }
 }
+
 // 버튼 클릭 이벤트
-let checkBtn = document.getElementById("checkBtn");
-checkBtn.addEventListener("click", check);
+const checkButton = document.getElementById("checkBtn");
+checkButton.addEventListener("click", checkPassword);
+
 // 비밀번호 생성 유효성 검사
-function validatePassword(newpassword) {
-  return RegExp.test(newpassword);
+function validatePassword(newPassword) {
+  return passwordRegex.test(newPassword);
 }
+
+// 뒤로가기 버튼 추가
+const backButton = document.getElementById("backButton");
+backButton.addEventListener("click", () => {
+  history.back();
+});
